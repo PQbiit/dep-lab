c4717175e3c44fb45ba18b3d14b63e700690729b
Alfonso Arriaga Quezada <luisaq95nin@gmail.com>
2021-10-07 14:19:11 -0500

    check css 3

--- server/index.js
+++ server/index.js
@@ -2,14 +2,15 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+app.use('/css', express.static(path.join(__dirname, '../styles/style.css')));
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, '../index.html'))
 });
 
-app.get('/css', (req, res) => {
-    res.sendFile(path.join(__dirname, '../styles/style.css'))
-});
+// app.get('/css', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../styles/style.css'))
+// });
 
 const port = process.env.PORT || 5050;
 
